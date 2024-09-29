d6766f72c13add062041c41303f9d6c6dbddc982
lucasSckenal <lucaspsckenal@gmail.com>
2024-09-28 22:55:26 -0300

    Refactor: minor changes

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Mainpage from "./Pages/Main/Mainpage";
+import "./global.scss";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Mainpage />
+  </StrictMode>
+);
